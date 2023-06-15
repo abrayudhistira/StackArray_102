1a02b1003eaaad39c66fc4cbef63f30ed181d822
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-06-15 14:57:18 +0700

    Add project files.

---
+++ MateriStack/MateriStack.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class StackArray {
+private:
+	string stack_array[5];
+	int top;
+public:
+	//constructor
+	StackArray() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {	//step 1
+			cout << "Number of data exceeds the limit." << endl;
+			return "";
+		}
+
+		top++; //step 2
+		stack_array[top] = element; //step 3
+	}
+
+};
