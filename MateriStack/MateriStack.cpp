f5b8718766c9f741f77c5ac2847b3b723d02ce6c
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-06-15 15:28:50 +0700

    class stack array

--- MateriStack/MateriStack.cpp
+++ MateriStack/MateriStack.cpp
@@ -1,24 +1,93 @@
-#include <iostream>
+﻿#include <iostream>
+#include <string>
 using namespace std;
 
-class StackArray {
+class StackArray {									//class stack array
 private:
 	string stack_array[5];
 	int top;
 public:
-	//constructor
+	
 	StackArray() {
 		top = -1;
 	}
 
 	string push(string element) {
-		if (top == 4) {	//step 1
+		if (top == 4) {				
 			cout << "Number of data exceeds the limit." << endl;
 			return "";
 		}
 
-		top++; //step 2
-		stack_array[top] = element; //step 3
+		top++;						
+		stack_array[top] = element; 
+		cout << endl;
+		cout << element << "ditambahkan(pushed)" << endl;
+
+		return element;
 	}
 
+	void pop() {
+		if (empty()) {				
+			cout << "\nStack is empty.Cannot pop." << endl;		
+			return;												
+		}
+
+		cout << "\nThe popped element is: " << stack_array[top] << endl; 
+		top--;															
+	}
+
+	
+	bool empty() {
+		return (top == -1);
+	}
+
+	void display() {
+		if (empty()) {
+			cout << "\nStack is empty." << endl;
+		}
+		else {
+			for (int tmp = 0; tmp <= top; tmp++) {
+				cout << stack_array[tmp] << endl;
+			}
+		}
+	}
 };
+
+int main() {
+	StackArray s;
+	while (true) {
+		cout << "\n###Stack Menu###\n";
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Display\n";
+		cout << "4. Exit\n";
+		cout << "Enter your choices: ";
+		string input;
+		getline(cin, input);
+		char ch = (input.empty() ? '0' : input[0]);
+		switch (ch) {
+		case '1': {
+			cout << "\nEnter a element: ";
+			string element;
+			getline(cin, element);
+			s.push(element);
+			break;
+		}
+		case '2':
+			if (s.empty()) {
+				cout << "\nStack is empty." << endl;
+				break;
+			}
+			s.pop();
+			break;
+		case '3':
+			s.display();
+			break;
+		case '4':
+			return 0;
+		default:
+			cout << "\nInvalid Choices." << endl;
+				break;
+		}
+	}
+}
