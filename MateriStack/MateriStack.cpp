b5533c3941075dd6918669659bd0ffa2a0b01367
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-06-15 15:32:53 +0700

    step 3

--- MateriStack/MateriStack.cpp
+++ MateriStack/MateriStack.cpp
@@ -19,7 +19,7 @@ public:
 		}
 
 		top++;						//step 2
-		stack_array[top] = element; 
+		stack_array[top] = element; //step 3
 		cout << endl;
 		cout << element << "ditambahkan(pushed)" << endl;
 
